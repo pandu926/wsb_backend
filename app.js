ae955f56fdf807962ed8e2575d81365a1901c7c3
pandu926 <63030609+pandu926@users.noreply.github.com>
2024-03-21 13:23:31 +0700

    ya

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = 8000;
+const port = 443;
 const cors = require("cors");
 const path = require("path");
 const swaggerUi = require("swagger-ui-express");
@@ -13,6 +13,14 @@ const gambarRoute = require("./src/routes/gambar.route");
 const tagRoute = require("./src/routes/tag.route");
 const tagPivotRoute = require("./src/routes/tagPivot.route");
 const adminRoute = require("./src/routes/admin.route");
+
+const fs = require("fs");
+const https = require("https");
+
+const options = {
+  key: fs.readFileSync("./ssl/private.key"),
+  cert: fs.readFileSync("./ssl/certificate.crt"),
+};
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
@@ -30,6 +38,6 @@ app.get("/", (req, res) => {
   return res.send("berhasil");
 });
 
-app.listen(port, () => {
-  console.log("website listen in port " + port);
+https.createServer(options, app).listen(port, function () {
+  console.log("Express server listening on port " + port);
 });
